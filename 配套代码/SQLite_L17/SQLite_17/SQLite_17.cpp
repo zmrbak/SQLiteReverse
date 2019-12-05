379696ad149d77886a18746350b3a9349fe9a012
zmrbak <20995658@qq.com>
2019-12-05 09:04:59 +0800

    上传源码

---
+++ 配套代码/SQLite_L17/SQLite_17/SQLite_17.cpp
@@ -0,0 +1,52 @@
+﻿// SQLite_17.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <stdio.h>
+int  f___(int a, int b, int c, int d)
+{
+	//printf("Hello World!\n");
+	return 0;
+}
+
+
+int __cdecl f___cdecl(int a,int b,int c,int d)
+{
+	//printf("Hello World!\n");
+	return 0;
+}
+
+int __stdcall f__stdcall(int a, int b, int c, int d)
+{
+	//printf("Hello World!\n");
+	return 0;
+}
+int __fastcall f__fastcall(int a, int b, int c, int d)
+{
+	//printf("Hello World!\n");
+	return 0;
+}
+
+int __vectorcall f__vectorcall(int a, int b, int c, int d)
+{
+	//printf("Hello World!\n");
+	return 0;
+}
+
+__declspec(naked) int f__naked(int a, int b, int c, int d)
+{
+	//printf("Hello World!\n");
+	__asm
+	{
+		mov eax,0
+	}
+}
+int main()
+{
+	//printf("Hello World!\n");
+	f___(1, 2, 3, 4);
+	f___cdecl(1,2,3,4);
+	f__stdcall(1, 2, 3, 4);
+	f__fastcall(1, 2, 3, 4);
+	f__vectorcall(1, 2, 3, 4);
+	f__naked(1, 2, 3, 4);
+}
